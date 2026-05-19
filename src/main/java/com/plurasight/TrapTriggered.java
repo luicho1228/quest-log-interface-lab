c17869663d436be2bbad0c1f13c604a36e57a348
luisvasquezgonzalez <luicho.vasquez44@gmail.com>
2026-05-19 12:36:54 -0400

    Created and implemented TrapTriggered.java

--- src/main/java/com/plurasight/TrapTriggered.java
+++ src/main/java/com/plurasight/TrapTriggered.java
@@ -1,4 +1,27 @@
 package com.plurasight;
 
-public class TrapTriggered {
+public class TrapTriggered implements ScoreEvent{
+    private String trapName;
+    private int damage;
+
+
+    public TrapTriggered(String trapName, int damage) {
+        this.trapName = trapName;
+        this.damage = damage;
+    }
+
+    @Override
+    public String getName() {
+        return trapName;
+    }
+
+    @Override
+    public int getPoints() {
+        return damage * -10;
+    }
+
+    @Override
+    public String getSummary() {
+        return String.format("Triggered %s for %d points",getName(),getPoints());
+    }
 }
