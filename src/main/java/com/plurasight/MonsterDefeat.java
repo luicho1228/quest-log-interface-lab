fb2558c2b67cbb372ece0ddfeb9ab3be031759aa
luisvasquezgonzalez <luicho.vasquez44@gmail.com>
2026-05-19 10:22:06 -0400

    Created and implemented TreasureFound.java class

--- src/main/java/com/plurasight/MonsterDefeat.java
+++ src/main/java/com/plurasight/MonsterDefeat.java
@@ -4,26 +4,25 @@ public class MonsterDefeat {
     private final String monsterName;
     private final int difficultyLevel;
     private final boolean boss;
-    private int points;
 
     public MonsterDefeat(String monsterName,int difficultyLevel, boolean boss){
         this.monsterName = monsterName;
         this.difficultyLevel = difficultyLevel;
         this.boss = boss;
-        points = 50;
     }
 
     public String getName(){
         return"";
     }
     public int getPoints(){
+        int points = 50;
         if(boss){
             points += 100;
         }
         return difficultyLevel * points;
     }
     public String getSummary(){
-        return String.format("Defeated %s for %d points",monsterName,points);
+        return String.format("Defeated %s for %d points",monsterName,getPoints());
     }
 
 }
--- src/main/java/com/plurasight/TreasureFound.java
+++ src/main/java/com/plurasight/TreasureFound.java
@@ -1,4 +1,30 @@
 package com.plurasight;
 
 public class TreasureFound {
+    private String treasureName;
+    private String rarity;
+
+    public String getName(){return treasureName;}
+    public int getPoints(){
+        int points;
+        switch (rarity){
+            case "common":
+                points = 25;
+                break;
+            case "rare":
+                points = 75;
+                break;
+            case "legendary":
+                points = 150;
+                break;
+            default:
+                points = 0;
+                break;
+        }
+        return points;
+    }
+    public String getSummary(){
+        return String.format("Found %s %s for %d points",rarity,treasureName,getPoints());
+    }
+
 }
