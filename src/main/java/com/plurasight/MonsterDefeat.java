a4f3b751c6e6dc62f6e039d5e7f631318754d995
luisvasquezgonzalez <luicho.vasquez44@gmail.com>
2026-05-19 10:14:33 -0400

    Created and implemented MonsterDefeat.java class

--- src/main/java/com/plurasight/MonsterDefeat.java
+++ src/main/java/com/plurasight/MonsterDefeat.java
@@ -1,4 +1,29 @@
 package com.plurasight;
 
 public class MonsterDefeat {
+    private final String monsterName;
+    private final int difficultyLevel;
+    private final boolean boss;
+    private int points;
+
+    public MonsterDefeat(String monsterName,int difficultyLevel, boolean boss){
+        this.monsterName = monsterName;
+        this.difficultyLevel = difficultyLevel;
+        this.boss = boss;
+        points = 50;
+    }
+
+    public String getName(){
+        return"";
+    }
+    public int getPoints(){
+        if(boss){
+            points += 100;
+        }
+        return difficultyLevel * points;
+    }
+    public String getSummary(){
+        return String.format("Defeated %s for %d points",monsterName,points);
+    }
+
 }
